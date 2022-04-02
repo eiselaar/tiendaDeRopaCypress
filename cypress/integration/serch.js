b656db3c96c86b272254379edcfb228d10964a6b
Emily Arias <iselaarias2002@gmail.com>
2022-04-01 20:54:25 -0600

    Solved T1 and T2

---
+++ cypress/integration/serch.js
@@ -0,0 +1,29 @@
+
+
+describe('Search elements', () => {
+    beforeEach(() => {
+        cy.visit('/');
+
+    })
+    it('search for elements for multiple results', () => {
+        cy.fixture('index').then((index) => {
+            cy.get(index.searchBox).type('dress');
+            cy.get(index.searchButton).click();
+        })
+        cy.fixture('searchResult').then((searchResult) => {
+            cy.get(searchResult.titule).should('contain', 'dress');
+
+        })
+    });
+    it('search for elements with no results', ()=>{
+        cy.fixture('index').then((index)=>{
+            cy.get(index.searchBox).type('qwerty');
+            cy.get(index.searchButton).click();
+        });
+        cy.fixture('searchResult').then((searchResult) => {
+            cy.get(searchResult.alert).should('contain', 'No results were found for your search');
+    
+        })
+    });
+  
+});
